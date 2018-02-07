63a7606f4d8917a09521f950a8502c627d6b8ed5
Mozillex <Mozillex@users.noreply.github.com>
2018-02-07 11:34:19 -0500

    css adjustments; syncing with codepen version

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var msgs = document.querySelector("#msgs");
-var today = document.querySelector("#today");
+var currentDiv = document.querySelector("#current");
 var forecast = document.querySelector("#forecast");
 var controls = document.querySelector("#controls");
 var zipVal,dif,lat,cel,lon,cVal = "a87a4b1d4";
 var togNum, togScale, tempNum, tempScale;
 var currentWeather ={};
 
+
 function displayElement(eID){
-   document.getElementById(eID).style.display = "block";
+   document.getElementById(eID).style.display = "inline-block";
 }
 
 function removeElement(eID){
@@ -21,7 +22,8 @@ function unhideElement(eID){
 // - - - GEOLOCATION for Weather
 function geoLoc(){
 
-   unhideElement("msgs");
+	unhideElement("msgs");
+	msgs.innerHTML = "sniffing out your location..."
 
 	if (!navigator.geolocation){// if geolocate unavailable, show zip-code input
 		msgs.innerHTML = "<h4>Your browser does not support geolocation.<br>Enter a 5-Digit US ZipCode Below</h4>";
@@ -32,19 +34,19 @@ function geoLoc(){
 		function success(position){
 			lat = position.coords.latitude;
 			lon = position.coords.longitude;
-			msgs.innerHTML = "<h4>Hang on while we look up your weather...</h4>";
+			msgs.innerHTML = "<h4>looking up your weather...</h4>";
 			apiCall("geo","lat="+lat+"&lon="+lon);
 		}
 		function error(error){
-			unhideElement("msgs");
 			msgs.innerHTML = "<h4>Geolocation is Unavailable.<br/>Enter a 5-digit U.S. ZipCode below.</h4>";
 			displayElement("zipCtrl");
 		}
 
+		removeElement("locate");
 		navigator.geolocation.getCurrentPosition(success,error);
 	}
 }
-geoLoc();
+// document.querySelector("#locOK").addEventHandler("checked",geoLoc);
 
 cell = "c3c4c47da"+cVal.split('').reverse().join('');
 
@@ -58,12 +60,11 @@ function submitZip() {
 	}
 	else {
 		zipVal = zipInput.value; //ZIP to SEND to weather API
-		//removeElement("zipCtrl");
-		document.querySelector("#msgs").textContent = "brb: Running outside to get your weather...";
+		removeElement("zipCtrl");
+		document.querySelector("#msgs").textContent = "looking out the window...";
 		apiCall("zip",zipVal);
 	}
-		//document.getElementById("zipInput").value = "";
-      zipInput.value = "";
+		document.getElementById("zipInput").value = "";
 }
 dif = "d=524d852309ce12"+cell;
 
@@ -71,21 +72,23 @@ dif = "d=524d852309ce12"+cell;
 // - - - - -
 function apiCall(mod,val){//api call by lat,lon coords
 
-   var modifier = (mod=="zip")?(mod + "=" + val + ",us"):val;
+  var modifier = (mod=="zip")?(mod + "=" + val + ",us"):val;
 	var owURL = "https://api.openweathermap.org/data/2.5/weather?" + modifier + "&appi"+dif;
-	var request = new XMLHttpRequest();
 
+	var request = new XMLHttpRequest();
 	request.open('GET',owURL);
 	request.responseType = 'json';
 	request.send();
-	request.onload = function() {
 
+	request.onload = function() {
 		currentWeather = request.response;
 		console.log(currentWeather);
 		printWeather(currentWeather);
 	}
 
 }
+
+var tempNum, tempScale;
 
 function printWeather(wO){
 
@@ -96,7 +99,7 @@ function printWeather(wO){
 	var tmpC = Math.round(tmpK-273.15);
 	tempNum = tmpF, tempScale = "F";
 
-	today.innerHTML = "<h4>Current Weather in "+ wO.name+ "</h4><h2>" + wO.weather[0].main + "</h2><h1><span id='togNum'>" + tempNum + "</span>\xB0<span id='togScale'>" + tempScale + "</span></h1><p>" + wO.weather[0].description + " and " + wO.main.humidity + "% humidity";
+	currentDiv.innerHTML = "<h4>Current Weather in "+ wO.name+ "</h4><h2>" + wO.weather[0].main + "</h2><h1><span id='togNum'>" + tempNum + "</span>\xB0<span id='togScale'>" + tempScale + "</span></h1><p>" + wO.weather[0].description + " and " + wO.main.humidity + "% humidity";
    togScale = document.getElementById("togScale");
    togNum = document.getElementById("togNum");
 
